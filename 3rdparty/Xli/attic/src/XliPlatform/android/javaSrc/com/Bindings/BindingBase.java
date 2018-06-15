0e6757b17237f69b9e5dea44359ae3aba86f6f20
Erik Faye-Lund <kusmabite@gmail.com>
2018-06-15 14:14:07 +0200

    remove unused attic-file

--- 3rdparty/Xli/attic/src/XliPlatform/android/javaSrc/com/Bindings/BindingBase.java
+++
@@ -1,5 +0,0 @@
-package com.Bindings;
-
-public final class BindingBase {
-
-}
--- 3rdparty/Xli/attic/src/XliPlatform/ios/CoreStream.mm
+++
@@ -1,5 +0,0 @@
-#import <CoreFoundation/CoreFoundation.h>
-#import <CoreServices/CoreServices.h>
-#import <Foundation/Foundation.h>
-#include <XliPlatform/PlatformSpecific/iOS.h>
-#include <Xli/Stream.h>
